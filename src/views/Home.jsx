e54f64ae9a8ed9ada2aaa2b1ef7ead522df86686
valen <83437771+valenmartini@users.noreply.github.com>
2022-08-09 10:33:33 -0300

    boton a personas y footer updated

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from "react";
 import { useEffect } from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router";
+
 
 
 export default function Home() {
 
+  const navigate = useNavigate();
   const isLoged = useSelector(state => state.login.isLoged);
   const userName = useSelector(state => state.login.userName);
 
+  const onClickIrPersonas = () => {
+    navigate('/personas');
+  }
+
   return (
     <div>
-      { isLoged && (
-        <h1>Bienvenido {userName}</h1>
+      { isLoged && ( 
+        <div>
+          <h1>Bienvenido {userName}</h1>
+          <Button onClick={onClickIrPersonas}>Ir a tabla de Personas</Button>
+        </div>
       )}
     </div>
   );
